5ffeb030cd4eade5b4bfbabf2f2e6e99d840d771
Daniel karnauch <danithec@hotmail.com>
2018-12-24 12:06:34 +0200

    Large text

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('path')
 
-
-
 const app = express()
 
-app.use('/', api)
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
 
 
 const port = 3000
